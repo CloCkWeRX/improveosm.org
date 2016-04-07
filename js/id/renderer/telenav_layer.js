90d30392d7693a88c2038f5f800daf355e0a16be
tudor.goia <tudor.goia@telenav.com>
2016-04-07 20:02:07 +0300

    WEBEU-719, WEBEU-697, WEBEU-696 - done with the first draft

--- js/id/renderer/telenav_layer.js
+++ js/id/renderer/telenav_layer.js
@@ -6,6 +6,14 @@ iD.TelenavLayer = function (context) {
         selectedItems = [],
         requestCount;
 
+    var types = {
+        dof: 'http://fcd-ss.skobbler.net:2680/directionOfFlowService_test/search',
+        mr: 'http://fcd-ss.skobbler.net:2680/missingGeoService_test/search',
+        tr: 'http://fcd-ss.skobbler.net:2680/turnRestrictionService_test/search'
+    };
+
+    var selectedTypes = [];
+
     // ==============================
     // ==============================
     // MapItem
@@ -235,6 +243,17 @@ iD.TelenavLayer = function (context) {
     };
 
     function render(selection) {
+
+        var zoom = Math.round(context.map().zoom());
+
+        if (zoom > 14) {
+            d3.select("#sidebar").classed('telenavPaneActive', enable);
+            d3.select(".pane-telenav").classed('hidden', !enable);
+        } else {
+            d3.select("#sidebar").classed('telenavPaneActive', false);
+            d3.select(".pane-telenav").classed('hidden', true);
+        }
+
         svg = selection.selectAll('svg')
             .data([0]);
 
@@ -274,16 +293,14 @@ iD.TelenavLayer = function (context) {
             requestQueue.length = 0;
         }
 
-        var zoom = Math.round(context.map().zoom());
-
         var boundingBoxUrlFragments = '?south=' +
             extent[0][1] + '&north=' + extent[1][1] + '&west=' +
             extent[0][0] + '&east=' + extent[1][0] + '&zoom=' + zoom;
 
         var requestUrlQueue = [];
-        requestUrlQueue.push('http://fcd-ss.skobbler.net:2680/directionOfFlowService_test/search' + boundingBoxUrlFragments);
-        requestUrlQueue.push('http://fcd-ss.skobbler.net:2680/missingGeoService_test/search' + boundingBoxUrlFragments);
-        requestUrlQueue.push('http://fcd-ss.skobbler.net:2680/turnRestrictionService_test/search' + boundingBoxUrlFragments);
+        for (var i = 0; i < selectedTypes.length; i++) {
+            requestUrlQueue.push(types[selectedTypes[i]] + boundingBoxUrlFragments);
+        }
 
         requestCount = requestUrlQueue.length;
         combinedItems.length = 0;
@@ -309,6 +326,248 @@ iD.TelenavLayer = function (context) {
         svg.dimensions(_);
         return render;
     };
+
+    var buildPane = function() {
+
+        var div = d3.selectAll('.pane_telenav')
+            .data([0]);
+
+        var enter = div.enter().append('div')
+            .attr('class', 'pane-telenav hidden');
+
+        var toggleEditModeContainer = enter.append('div')
+            .attr('class', 'toggleEditModeContainer');
+
+        toggleEditModeContainer.append('input')
+            .attr('type', 'checkbox')
+            .attr('id', 'toggleEditMode')
+            .attr('class', 'tel_displayBlock');
+        toggleEditModeContainer.append('label')
+            .attr('for', 'toggleEditMode')
+            .text('Toggle Edit Mode');
+
+        var $statusContainer = enter.append('div')
+            .attr('id', 'STATUS')
+            .attr('class', 'tel_displayBlock')
+            .text('Reported Status');
+
+        var $statusForm = $statusContainer.append('form')
+            .attr('class', 'filterForm');
+
+        var $statusDivOpen = $statusForm.append('div')
+            .attr('class', 'tel_displayInline');
+        var $statusLabelOpen = $statusDivOpen.append('label')
+            .attr('for', 'OPEN')
+            .text('open');
+
+        var statusInputOpen = $statusDivOpen.append('input')
+            .attr('type', 'radio')
+            .attr('id', 'OPEN')
+            .attr('class', 'filterItem')
+            .attr('name', 'filter');
+        var $statusDivSolved = $statusForm.append('div')
+            .attr('class', 'tel_displayInline');
+        var $statusLabelSolved = $statusDivSolved.append('label')
+            .attr('for', 'SOLVED')
+            .text('solved');
+
+        var statusInputSolved = $statusLabelSolved.append('input')
+            .attr('type', 'radio')
+            .attr('id', 'SOLVED')
+            .attr('class', 'filterItem')
+            .attr('name', 'filter');
+
+        var $statusDivInvalid = $statusForm.append('div')
+            .attr('class', 'tel_displayInline');
+        var $statusLabelInvalid = $statusDivInvalid.append('label')
+            .attr('for', 'INVALID')
+            .text('invalid');
+
+        var statusInputInvalid = $statusDivInvalid.append('input')
+            .attr('type', 'radio')
+            .attr('id', 'INVALID')
+            .attr('class', 'filterItem')
+            .attr('name', 'filter');
+
+
+
+        var $directionFilterContainer = enter.append('div')
+            .attr('id', 'DIRECTION_FILTER')
+            .attr('class', 'tel_displayBlock');
+
+        $directionFilterContainer.append('input')
+            .attr('type', 'checkbox')
+            .attr('class', 'filterActivation')
+            .attr('id', 'oneWayConfidence');
+
+        var $directionFilterLabel = $directionFilterContainer.append('label')
+            .attr('class', 'sectionHeader')
+            .attr('for', 'oneWayConfidence')
+            .text('One-way confidence');
+
+        var $directionFilterForm = $directionFilterContainer.append('form')
+            .attr('class', 'typeForm');
+        var $direction_highlyProbableContainer = $directionFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $direction_highlyProbableContainer.append('input')
+            .attr('id', 'C1')
+            .attr('type', 'checkbox');
+        $direction_highlyProbableContainer.append('label')
+            .attr('for', 'C1')
+            .text('Highly Probable');
+        var $direction_mostLikelyContainer = $directionFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $direction_mostLikelyContainer.append('input')
+            .attr('id', 'C2')
+            .attr('type', 'checkbox');
+        $direction_mostLikelyContainer.append('label')
+            .attr('for', 'C2')
+            .text('Most Likely');
+        var $direction_probableContainer = $directionFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $direction_probableContainer.append('input')
+            .attr('id', 'C3')
+            .attr('type', 'checkbox');
+        $direction_probableContainer.append('label')
+            .attr('for', 'C3')
+            .text('Probable');
+
+
+        var $missingFilterContainer = enter.append('div')
+            .attr('id', 'MISSING_FILTER')
+            .attr('class', 'tel_displayBlock');
+
+        $missingFilterContainer                    .append('input')
+            .attr('type', 'checkbox')
+            .attr('class', 'filterActivation')
+            .attr('id', 'missingRoadType');
+
+        var $missingFilterLabel = $missingFilterContainer.append('label')
+            .attr('class', 'sectionHeader')
+            .attr('for', 'missingRoadType')
+            .text('Missing road type');
+
+        var $missingFilterForm = $missingFilterContainer.append('form')
+            .attr('class', 'typeForm');
+        var $missing_roadContainer = $missingFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $missing_roadContainer.append('input')
+            .attr('id', 'ROAD')
+            .attr('type', 'checkbox');
+        $missing_roadContainer.append('label')
+            .attr('for', 'ROAD')
+            .text('Road');
+        var $missing_parkingContainer = $missingFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $missing_parkingContainer.append('input')
+            .attr('id', 'PARKING')
+            .attr('type', 'checkbox');
+        $missing_parkingContainer.append('label')
+            .attr('for', 'PARKING')
+            .text('Parking');
+        var $missing_bothContainerContainer = $missingFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $missing_bothContainerContainer.append('input')
+            .attr('id', 'BOTH')
+            .attr('type', 'checkbox');
+        $missing_bothContainerContainer.append('label')
+            .attr('for', 'BOTH')
+            .text('Both');
+
+        $missingFilterForm.append('p')
+            .attr('class', 'sectionHeader tel_displayBlock')
+            .text('Filters');
+
+        var $missing_waterContainer = $missingFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $missing_waterContainer.append('input')
+            .attr('id', 'WATER')
+            .attr('type', 'checkbox');
+        $missing_waterContainer.append('label')
+            .attr('for', 'WATER')
+            .text('Water Trail');
+
+        var $missing_pathContainer = $missingFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $missing_pathContainer.append('input')
+            .attr('id', 'PATH')
+            .attr('type', 'checkbox');
+        $missing_pathContainer.append('label')
+            .attr('for', 'PATH')
+            .text('Path Trail');
+
+
+        var $restrictionFilterContainer = enter.append('div')
+            .attr('id', 'RESTRICTION_FILTER')
+            .attr('class', 'tel_displayBlock');
+
+        $restrictionFilterContainer                    .append('input')
+            .attr('type', 'checkbox')
+            .attr('class', 'filterActivation')
+            .attr('id', 'turnRestrictionConfidence');
+
+        var $restrictionFilterLabel = $restrictionFilterContainer.append('label')
+            .attr('class', 'sectionHeader')
+            .attr('for', 'turnRestrictionConfidence')
+            .text('Turn Restriction confidence');
+
+        var $restrictionFilterForm = $restrictionFilterContainer.append('form')
+            .attr('class', 'typeForm');
+
+        var $restriction_highlyProbableContainer = $restrictionFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $restriction_highlyProbableContainer.append('input')
+            .attr('id', 'C1')
+            .attr('type', 'checkbox');
+        $restriction_highlyProbableContainer.append('label')
+            .attr('for', 'C1')
+            .text('Highly Probable');
+
+        var $restriction_probableContainer = $restrictionFilterForm.append('div')
+            .attr('class', 'tel_displayInline');
+        $restriction_probableContainer.append('input')
+            .attr('id', 'C2')
+            .attr('type', 'checkbox');
+        $restriction_probableContainer.append('label')
+            .attr('for', 'C2')
+            .text('Probable');
+
+        d3.select('.toggleEditModeContainer').on('click', function() {
+            if (d3.select('.layer-telenav').classed('editMode')) {
+                d3.select('.layer-telenav').classed('editMode', false);
+            } else {
+                d3.select('.layer-telenav').classed('editMode', true);
+            }
+        });
+
+        d3.select('#oneWayConfidence').on('click', function() {
+            if (d3.select('#oneWayConfidence').property('checked')) {
+                selectedTypes.push('dof');
+            } else {
+                selectedTypes.splice(selectedTypes.indexOf('dof'), 1);
+            }
+            render(d3.select('.layer-telenav'));
+        });
+
+        d3.select('#missingRoadType').on('click', function() {
+            if (d3.select('#missingRoadType').property('checked')) {
+                selectedTypes.push('mr');
+            } else {
+                selectedTypes.splice(selectedTypes.indexOf('mr'), 1);
+            }
+            render(d3.select('.layer-telenav'));
+        });
+
+        d3.select('#turnRestrictionConfidence').on('click', function() {
+            if (d3.select('#turnRestrictionConfidence').property('checked')) {
+                selectedTypes.push('tr');
+            } else {
+                selectedTypes.splice(selectedTypes.indexOf('tr'), 1);
+            }
+            render(d3.select('.layer-telenav'));
+        });
+
+    }();
 
     return render;
 };
