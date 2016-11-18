66c9e7fffb983745d2d0b8698fbc0eb9a966fe6a
tudor.goia <tudorg@telenav.com>
2016-11-18 12:00:07 +0200

    WEBEU-1275 test APIs are used in the production deployment created separate git-versioned deployment scripts

---
+++ deploy_prod.sh
@@ -0,0 +1,18 @@
+npm install
+make
+# the softlink needing repairing problem
+ln -nfs dist/land.html land.html
+DATE=$(date +"%s")
+echo ${DATE}
+sed  -i 's/var PRODUCTION_API_PATHS = false\;/var PRODUCTION_API_PATHS = true\;/' js/id/renderer/telenav_layer.js
+sed -i "s/map.css/map-${DATE}.css/" index.html
+sed -i "s/app.css/app-${DATE}.css/" index.html
+sed -i "s/telenav_pane.js/telenav_pane-${DATE}.js/" index.html
+sed -i "s/telenav_layer.js/telenav_layer-${DATE}.js/" index.html
+mv css/map.css css/map-${DATE}.css
+mv css/app.css css/app-${DATE}.css
+rm -rf js/id/renderer/telenav_pane-*.js
+mv js/id/renderer/telenav_pane.js js/id/renderer/telenav_pane-${DATE}.js
+rm -rf js/id/renderer/telenav_layer-*.js
+mv js/id/renderer/telenav_layer.js js/id/renderer/telenav_layer-${DATE}.js
+chown -R apache. ${WORKSPACE}
